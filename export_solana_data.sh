396e7458ef6fafb13f246d505cf4b68870ac9df4
root <root@LAPTOP-E3023STO>
2024-12-08 15:47:47 +0800

    ethereum and solana plotting function, data retrieving script and gnuplot function implemented

--- export_solana_data.sh
+++ export_solana_data.sh
@@ -3,20 +3,20 @@
 MYSQL_USER="root"
 MYSQL_PASS=""
 MYSQL_DB="bitcoin_information"
-OUTPUT_FILE="/mnt/c/Users/Asus/data_management_cw2_ethan_kevin/bitcoin_data.csv"
+OUTPUT_FILE="/mnt/c/Users/Asus/data_management_cw2_ethan_kevin/solana_data.csv"
 
 # Temporary file path for MySQL OUTFILE
-TEMP_FILE="/var/lib/mysql-files/temp_bitcoin_data.csv"
+TEMP_FILE="/var/lib/mysql-files/temp_solana_data.csv"
 
 echo "Executing as: $(whoami)"
 
 if [ -f "$TEMP_FILE" ]; then
-	echo "Removing existing temporary file..."
-	sudo rm "$TEMP_FILE"
-	if [ $? -ne 0 ]; then
-		echo "Failed to remove $TEMP_FILE. Exiting."
-		exit 1
-	fi
+        echo "Removing existing temporary file..."
+        sudo rm "$TEMP_FILE"
+        if [ $? -ne 0 ]; then
+                echo "Failed to remove $TEMP_FILE. Exiting."
+                exit 1
+        fi
 fi
 
 # Export MySQL data to CSV
@@ -26,11 +26,10 @@ INTO OUTFILE '$TEMP_FILE'
 FIELDS TERMINATED BY ','
 ENCLOSED BY '\"'
 LINES TERMINATED BY '\n'
-FROM bitcoin;" || { echo "MYSQL export failed"; exit 1;}
+FROM solana;" || { echo "MYSQL export failed"; exit 1;}
 
 
 # Move the exported file to target directory
 sudo mv "$TEMP_FILE" "$OUTPUT_FILE" || { echo "Failed to move file to $OUTPUT_FILE"; exit 1;}
 
 echo "Export successful: $OUTPUT_FILE"
-
